887fbfcb1f9f1317264ff4a65fbc69dd4731e458
shubhamb512 <shubham.b@connectingdotsinfotech.com>
2023-03-13 14:00:06 +0530

    added waitress server

--- main.py
+++ main.py
@@ -492,7 +492,8 @@ def insertintotable():
                                forecast_set=forecast_set,error_lr=round(error_lr,2),error_lstm=round(error_lstm,2),error_arima=round(error_arima,2))
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port='5000')
+    from waitress import serve
+    serve(app, host='127.0.0.1', port=5000)
    
 
 
