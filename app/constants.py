cda05971d35ceb13ae3f0e13d020e604bf928312
shubhamb512 <shubham.b@connectingdotsinfotech.com>
2023-03-13 10:33:50 +0530

    initial commit

---
+++ app/constants.py
@@ -0,0 +1,7 @@
+consumer_key= 'FBf7OEUaTTQ5fpc4RUHiXCUUm'
+consumer_secret= 'iEj4CVbCk7mWZara8e1PpmN1jckWy9nRHZfvh2tPo67SAjJdRl'
+
+access_token='1388042454272671748-qU1o3EabX64FVKpuw7r4QXzUQN8i4j'
+access_token_secret='Zg4mI2rczQPpQiN7aN3rDAWTM2ULbA9xcquhPBeEQq2w2'
+
+num_of_tweets = int(300)
