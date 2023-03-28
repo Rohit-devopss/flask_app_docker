831b1a5108891c1b8c7d65c7d8c0194fe534be85
shubhamborkar512 <shubhamborkar512@gmail.com>
2023-03-28 10:25:42 +0530

    added threads

--- main.py
+++ main.py
@@ -448,7 +448,7 @@ def insertintotable():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000,threads=7)
     # app.run()
 
    
